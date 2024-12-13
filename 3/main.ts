f48af85f987e8339ae264f681def43e33945e9a5
Ohgodwhydidyoutakeit <w.lipowski97@gmail.com>
2024-12-13 12:29:59 +0100

    feat(day3): part 2

--- 3/main.ts
+++ 3/main.ts
@@ -1,14 +1,16 @@
 
 const INPUT_PATH = "./input.txt";
 
+const file = readFile();
 
 
-(function () {
+solution1(); //167650499
+solution2(); // 95846796
 
-  const file = readFile();
+function solution1() {
 
   try {
-    const parsedInput = parseInput(file)
+    const parsedInput = parseInputMut(file)
 
     if (!parsedInput) {
       throw new Error("There are no matches in provided input")
@@ -25,11 +27,57 @@ const INPUT_PATH = "./input.txt";
   } catch (error) {
     console.error(error)
   }
+}
 
-}())
+function solution2() {
+  const parsedInput = parseInputWithDoAndDontMut(file)
+
+  const checkedArray = dosArray(parsedInput)
+
+  const extractedData = extractData(checkedArray)
+
+  const multipliedArray = multiplyInsideArray(extractedData)
+
+  const solution2 = addAndReduce(multipliedArray)
+
+  console.log(solution2)
+
+
+}
 
 
 
+
+function dosArray(inputArray: string[]): string[] {
+  const startString = "do()";
+  const stopString = "don't()";
+
+  let shouldBePushed: boolean = true;
+  const temporaryArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    const value = inputArray[i]
+
+    if (value === stopString) {
+      shouldBePushed = false
+      continue;
+    }
+
+    if (value === startString) {
+      shouldBePushed = true
+      continue;
+    }
+
+    if (shouldBePushed) {
+      temporaryArray.push(value)
+    } else {
+      continue
+    }
+
+  }
+
+
+  return temporaryArray
+}
 
 function addAndReduce(inputArray: number[]): number {
   return inputArray.reduce((a, b) => a + b)
@@ -41,7 +89,7 @@ function multiplyInsideArray(inputArray: [number, number][]) {
   })
 }
 
-function extractData(input: RegExpMatchArray): [number, number][] {
+function extractData(input: RegExpMatchArray | string[]): [number, number][] {
   const numberArray: [number, number][] = input.map((element) => {
     return getDigits(element)
   })
@@ -55,7 +103,14 @@ function getDigits(input: string): [number, number] {
   return [+matches![0], +matches![1]]
 }
 
-function parseInput(input: string): RegExpMatchArray | null {
+
+function parseInputWithDoAndDontMut(input: string): string[] {
+  // const regex = /\d+|do\(\)|don't\(\)/g;
+  const regex = /mul\(\d+,\d+\)|do\(\)|don't\(\)/g;
+  const matches = input.match(regex);
+  return matches ? matches.map(item => item.includes('do') || item.includes('don\'t') ? item : item) : [];
+}
+function parseInputMut(input: string): RegExpMatchArray | null {
   const regex = /mul\((\d+),(\d+)\)/g;
   return input.match(regex)
 }
